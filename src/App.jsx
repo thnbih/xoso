e2fb99eb21bb4a92d786add9ef136d3446b235c6
thnbih <22520136@gm.uit.edu.vn>
2024-12-02 01:12:53 +0700

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
   // Hàm xử lý sự kiện khi nhấn nút
   const handleRandomNumber = () => {
-    const randomNumber = Math.floor(Math.random() * 10001); // Random từ 0 đến 10000
+    const randomNumber = Math.floor(Math.random() * 1001); // Random từ 0 đến 10000
 
     // Kiểm tra số random
-    if (randomNumber === 6789) {
+    if (randomNumber === 678) {
       alert(`Chúc mừng! Bạn đã quay được số ${randomNumber}.`);
     } else {
       alert(`Kết quả: ${randomNumber}. Quay số lại!`);
